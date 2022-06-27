dec9bfe91394d76900340616bbc7e30476be01bc
Documenter.jl <documenter@juliadocs.github.io>
2022-06-27 12:10:26 +0000

    build based on 0e3167a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Sniper","category":"page"},{"location":"#Sniper","page":"Home","title":"Sniper","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Sniper.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Sniper]","category":"page"}]
+}
