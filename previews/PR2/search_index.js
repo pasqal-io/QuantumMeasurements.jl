3864efbc76c904eabb5e3a6e7b8d72f38c6f9244
Documenter.jl <documenter@juliadocs.github.io>
2022-07-04 07:27:07 +0000

    build based on f158711

---
+++ previews/PR2/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = QuantumMeasurements","category":"page"},{"location":"#QuantumMeasurements","page":"Home","title":"QuantumMeasurements","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QuantumMeasurements.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [QuantumMeasurements]","category":"page"}]
+}
